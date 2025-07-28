8ac1595ccb4d0da58609ec50f774897cbea2a3a4
Rahul Sharma <rahul@rstforum.net>
2025-07-28 20:15:04 +0530

    Step 7 completed:     - Added 404 page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Layout from '@components/Layout';
+import ErrorScreen from '@screens/Error';
 import HomeScreen from '@screens/Home';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <Layout />,
+		errorElement: <ErrorScreen />,
 		children: [
 			{
 				index: true,

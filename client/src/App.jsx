7e1333a94e41c2c1e1e4492baf32c06f5a26a7d8
Rahul Sharma <rahul@rstforum.net>
2025-08-22 20:24:47 +0530

    Step 56 completed:     - Made profile page     - Added update profile functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import OrderScreen from '@screens/Order';
 import PaymentScreen from '@screens/PaymentScreen';
 import PlaceOrderScreen from '@screens/PlaceOrder';
 import ProductDetails from '@screens/ProductDetails';
+import ProfileScreen from '@screens/Profile';
 import RegisterScreen from '@screens/Register';
 import ShippingScreen from '@screens/Shipping';
 import store from './store';
@@ -63,6 +64,10 @@ const router = createBrowserRouter([
 					{
 						path: '/order/:id',
 						element: <OrderScreen />,
+					},
+					{
+						path: '/profile',
+						element: <ProfileScreen />,
 					},
 				],
 			},

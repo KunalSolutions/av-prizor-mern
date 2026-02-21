de4113cfd2a7e6f0d92b2a3a30ef89b55131d341
Kunal <carrierhub41@gmail.com>
2026-02-21 15:02:10 +0530

    Update constants.js

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,7 +1,8 @@
-export const BASE_URL = 'https://av-prizor-backend.onrender.com';
+export const BASE_URL = 'https://av-prizor-backend.onrender.com/api/v1';
 export const PRODUCTS_URL = '/products';
 export const USERS_URL = '/users';
 export const ORDERS_URL = '/orders';
 export const PAYPAL_URL = '/config/paypal';
 export const UPLOADS_URL = '/upload';
+
 
